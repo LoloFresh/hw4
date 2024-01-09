20010acbe469dd9d7fcaa14ddeaa6a7b06b92711
334905 <334905@niuitmo.ru>
2024-01-09 10:32:24 +0300

    TreeBimap is now ALV

--- src/collections/TreeBimap.java
+++ src/collections/TreeBimap.java
@@ -8,12 +8,14 @@ public class TreeBimap implements Bimap {
         Node left;
         Node right;
         Node parent;
+        int height;
 
         public Half(final String key, final Node parent) {
             this.key = key;
             this.left = null;
             this.right = null;
             this.parent = parent;
+            this.height = 1;
         }
     }
 
@@ -26,11 +28,31 @@ public class TreeBimap implements Bimap {
             rightHalf = new Half(rightK, rightP);
         }
 
+        private static int leftHeight(final Node node) {
+            return node == null ? 0 : node.leftHalf.height;
+        }
+
+        private static int rightHeight(final Node node) {
+            return node == null ? 0 : node.rightHalf.height;
+        }
+
+        private void leftFixHeight() {
+            leftHalf.height = Math.max(leftHeight(leftHalf.left), leftHeight(leftHalf.right)) + 1;
+        }
+
+        private void rightFixHeight() {
+            rightHalf.height = Math.max(rightHeight(rightHalf.left), rightHeight(rightHalf.right)) + 1;
+        }
+
         public boolean checkInvariant(final Comparator<? super String> leftCmp, final Comparator<? super String> rightCmp) {
             return (leftHalf.left == null || leftHalf.left.leftHalf.parent == this && leftCmp.compare(leftHalf.left.leftHalf.key, leftHalf.key) < 0 && leftHalf.left.checkInvariant(leftCmp, rightCmp))
                     && (leftHalf.right == null || leftHalf.right.leftHalf.parent == this && leftCmp.compare(leftHalf.key, leftHalf.right.leftHalf.key) < 0 && leftHalf.right.checkInvariant(leftCmp, rightCmp))
                     && (rightHalf.left == null || rightHalf.left.rightHalf.parent == this && rightCmp.compare(rightHalf.left.rightHalf.key, rightHalf.key) < 0)
-                    && (rightHalf.right == null || rightHalf.right.rightHalf.parent == this && rightCmp.compare(rightHalf.key, rightHalf.right.rightHalf.key) < 0);
+                    && (rightHalf.right == null || rightHalf.right.rightHalf.parent == this && rightCmp.compare(rightHalf.key, rightHalf.right.rightHalf.key) < 0)
+                    && leftHalf.height == Math.max(leftHeight(leftHalf.left), leftHeight(leftHalf.right)) + 1
+                    && Math.abs(leftHeight(leftHalf.left) - leftHeight(leftHalf.right)) <= 1
+                    && rightHalf.height == Math.max(rightHeight(rightHalf.left), rightHeight(rightHalf.right)) + 1
+                    && Math.abs(rightHeight(rightHalf.left) - rightHeight(rightHalf.right)) <= 1;
         }
 
         public Node leftMin() {
@@ -112,6 +134,154 @@ public class TreeBimap implements Bimap {
                 return that.rightHalf.parent;
             }
         }
+
+        private void leftReplaceWith(final Node other) {
+            assert leftHalf.parent != null;
+            if (this == leftHalf.parent.leftHalf.left) {
+                leftHalf.parent.leftHalf.left = other;
+            } else {
+                assert this == leftHalf.parent.leftHalf.right;
+                leftHalf.parent.leftHalf.right = other;
+            }
+            other.leftHalf.parent = leftHalf.parent;
+        }
+
+        private void rightReplaceWith(final Node other) {
+            assert rightHalf.parent != null;
+            if (this == rightHalf.parent.rightHalf.left) {
+                rightHalf.parent.rightHalf.left = other;
+            } else {
+                assert this == rightHalf.parent.rightHalf.right;
+                rightHalf.parent.rightHalf.right = other;
+            }
+            other.rightHalf.parent = rightHalf.parent;
+        }
+
+        private void leftSetLeft(final Node newLeft) {
+            leftHalf.left = newLeft;
+            if (newLeft != null) {
+                newLeft.leftHalf.parent = this;
+            }
+        }
+
+        private void rightSetLeft(final Node newLeft) {
+            rightHalf.left = newLeft;
+            if (newLeft != null) {
+                newLeft.rightHalf.parent = this;
+            }
+        }
+
+        private void leftSetRight(final Node newRight) {
+            leftHalf.right = newRight;
+            if (newRight != null) {
+                newRight.leftHalf.parent = this;
+            }
+        }
+
+        private void rightSetRight(final Node newRight) {
+            rightHalf.right = newRight;
+            if (newRight != null) {
+                newRight.rightHalf.parent = this;
+            }
+        }
+
+        private void leftRotateLeft() {
+            final Node r = leftHalf.right;
+            leftReplaceWith(r);
+            leftSetRight(r.leftHalf.left);
+            r.leftSetLeft(this);
+            leftFixHeight();
+            r.leftFixHeight();
+        }
+
+        private void rightRotateLeft() {
+            final Node r = rightHalf.right;
+            rightReplaceWith(r);
+            rightSetRight(r.rightHalf.left);
+            r.rightSetLeft(this);
+            rightFixHeight();
+            r.rightFixHeight();
+        }
+
+        private void leftRotateRight() {
+            final Node l = leftHalf.left;
+            leftReplaceWith(l);
+            leftSetLeft(l.leftHalf.right);
+            l.leftSetRight(this);
+            leftFixHeight();
+            l.leftFixHeight();
+        }
+
+        private void rightRotateRight() {
+            final Node l = rightHalf.left;
+            rightReplaceWith(l);
+            rightSetLeft(l.rightHalf.right);
+            l.rightSetRight(this);
+            rightFixHeight();
+            l.rightFixHeight();
+        }
+
+        public void leftFix() {
+            Node that = this;
+            while (that.leftHalf.parent != null) {
+                final int disbalance = leftHeight(that.leftHalf.left) - leftHeight(that.leftHalf.right);
+                switch (disbalance) {
+                    case -1, 0, 1:
+                        that.leftFixHeight();
+                        break;
+                    case -2:
+                        if (leftHeight(that.leftHalf.right.leftHalf.right) == leftHeight(that.leftHalf.right) - 1) {
+                            that.leftRotateLeft();
+                        } else {
+                            that.leftHalf.right.leftRotateRight();
+                            that.leftRotateLeft();
+                        }
+                        break;
+                    case 2:
+                        if (leftHeight(that.leftHalf.left.leftHalf.left) == leftHeight(that.leftHalf.left) - 1) {
+                            that.leftRotateRight();
+                        } else {
+                            that.leftHalf.left.leftRotateLeft();
+                            that.leftRotateRight();
+                        }
+                        break;
+                    default:
+                        throw new AssertionError("left balance of " + that + " is equal to " + disbalance);
+                }
+                that = that.leftHalf.parent;
+            }
+        }
+
+        public void rightFix() {
+            Node that = this;
+            while (that.rightHalf.parent != null) {
+                final int disbalance = rightHeight(that.rightHalf.left) - rightHeight(that.rightHalf.right);
+                switch (disbalance) {
+                    case -1, 0, 1:
+                        that.rightFixHeight();
+                        break;
+                    case -2:
+                        if (rightHeight(that.rightHalf.right.rightHalf.right) == rightHeight(that.rightHalf.right) - 1) {
+                            that.rightRotateLeft();
+                        } else {
+                            that.rightHalf.right.rightRotateRight();
+                            that.rightRotateLeft();
+                        }
+                        break;
+                    case 2:
+                        if (rightHeight(that.rightHalf.left.rightHalf.left) == rightHeight(that.rightHalf.left) - 1) {
+                            that.rightRotateRight();
+                        } else {
+                            that.rightHalf.left.rightRotateLeft();
+                            that.rightRotateRight();
+                        }
+                        break;
+                    default:
+                        throw new AssertionError("right balance of " + that + " is equal to " + disbalance);
+                }
+                that = that.rightHalf.parent;
+            }
+        }
     }
 
     private final Comparator<? super String> leftCmp;
@@ -175,11 +345,18 @@ public class TreeBimap implements Bimap {
 
     private void remove(final Node node) {
         final Node leftToUnlink;
+        final Node leftToFix;
         if (node.leftHalf.right != null && node.leftHalf.left != null) {
             leftToUnlink = node.leftHalf.right.leftMin();
             assert leftToUnlink.leftHalf.left == null;
+            if (leftToUnlink.leftHalf.parent != node) {
+                leftToFix = leftToUnlink.leftHalf.parent;
+            } else {
+                leftToFix = leftToUnlink;
+            }
         } else {
             leftToUnlink = node;
+            leftToFix = node.leftHalf.parent;
         }
         if (leftToUnlink.leftHalf.right == null) {
             if (leftToUnlink.leftHalf.parent.leftHalf.left == leftToUnlink) {
@@ -218,11 +395,18 @@ public class TreeBimap implements Bimap {
         }
 
         final Node rightToUnlink;
+        final Node rightToFix;
         if (node.rightHalf.right != null && node.rightHalf.left != null) {
             rightToUnlink = node.rightHalf.right.rightMin();
             assert rightToUnlink.rightHalf.left == null;
+            if (rightToUnlink.rightHalf.parent != node) {
+                rightToFix = rightToUnlink.rightHalf.parent;
+            } else {
+                rightToFix = rightToUnlink;
+            }
         } else {
             rightToUnlink = node;
+            rightToFix = rightToUnlink.rightHalf.parent;
         }
         if (rightToUnlink.rightHalf.right == null) {
             if (rightToUnlink.rightHalf.parent.rightHalf.left == rightToUnlink) {
@@ -259,6 +443,9 @@ public class TreeBimap implements Bimap {
             }
             node.rightHalf.left = node.rightHalf.right = node.rightHalf.parent = null;
         }
+
+        leftToFix.leftFix();
+        rightToFix.rightFix();
 
         size--;
     }
@@ -343,6 +530,8 @@ public class TreeBimap implements Bimap {
             rightNode.rightHalf.right = newNode;
         }
 
+        leftNode.leftFix();
+        rightNode.rightFix();
         size++;
     }
 
@@ -440,8 +629,7 @@ public class TreeBimap implements Bimap {
 
                     @Override
                     public boolean contains(final Object o) {
-                        @SuppressWarnings("unchecked")
-                        final Entry<String, String> entry = (Entry<String, String>) o;
+                        @SuppressWarnings("unchecked") final Entry<String, String> entry = (Entry<String, String>) o;
                         final Node node = TreeBimap.this.leftFind(entry.getKey());
                         return node != null && rightCmp.compare(node.rightHalf.key, entry.getValue()) == 0;
                     }
@@ -458,8 +646,7 @@ public class TreeBimap implements Bimap {
 
                     @Override
                     public boolean remove(final Object o) {
-                        @SuppressWarnings("unchecked")
-                        final Entry<String, String> entry = (Entry<String, String>) o;
+                        @SuppressWarnings("unchecked") final Entry<String, String> entry = (Entry<String, String>) o;
                         final Node node = TreeBimap.this.leftFind(entry.getKey());
                         if (node != null && rightCmp.compare(node.rightHalf.key, entry.getValue()) == 0) {
                             TreeBimap.this.remove(node);
@@ -539,8 +726,7 @@ public class TreeBimap implements Bimap {
 
                     @Override
                     public boolean contains(final Object o) {
-                        @SuppressWarnings("unchecked")
-                        final Entry<String, String> entry = (Entry<String, String>) o;
+                        @SuppressWarnings("unchecked") final Entry<String, String> entry = (Entry<String, String>) o;
                         final Node node = TreeBimap.this.rightFind(entry.getValue());
                         return node != null && leftCmp.compare(node.leftHalf.key, entry.getKey()) == 0;
                     }
@@ -557,8 +743,7 @@ public class TreeBimap implements Bimap {
 
                     @Override
                     public boolean remove(final Object o) {
-                        @SuppressWarnings("unchecked")
-                        final Entry<String, String> entry = (Entry<String, String>) o;
+                        @SuppressWarnings("unchecked") final Entry<String, String> entry = (Entry<String, String>) o;
                         final Node node = TreeBimap.this.rightFind(entry.getValue());
                         if (node != null && leftCmp.compare(node.leftHalf.key, entry.getKey()) == 0) {
                             TreeBimap.this.remove(node);
